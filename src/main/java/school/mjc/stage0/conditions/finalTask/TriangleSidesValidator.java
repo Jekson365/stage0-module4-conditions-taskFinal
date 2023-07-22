b1dae9ae546267ac81f20603ee18f0876a9e4419
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-07-22 13:20:01 +0400

    dh

--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+        if (firstSide + secondSide > thirdSide) {
+            System.out.println("this is a valid triangle");
+        }
+        if (firstSide + thirdSide > secondSide) {
+            System.out.println("this is a valid triangle");
+        }
+        if (secondSide + thirdSide > firstSide) {
+            System.out.println("this is a valid triangle");
+        }
+        else {
+            System.out.println("it's not a triangle");
+        }
     }
 }
