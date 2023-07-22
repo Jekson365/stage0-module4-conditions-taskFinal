630bbbc4721b02611eb6bbcb5ea21a29b2a64179
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-07-22 14:00:29 +0400

    dh

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -5,52 +5,57 @@ public class DaysInMonth {
         if (year > 0) {
 
         if (year % 4 == 0) {
-            switch (month) {
-                case 1:
-                    System.out.println(31);
-                    break;
-                case 2:
-                    System.out.println(28);
-                    break;
-                case 3:
-                    System.out.println(31);
-                    break;
-                case 4:
-                    System.out.println(30);
-                    break;
-                case 5:
-                    System.out.println(31);
-                    break;
-                case 6:
-                    System.out.println(30);
-                    break;
-                case 7:
-                    System.out.println(31);
-                    break;
-                case 8:
-                    System.out.println(31);
-                    break;
-                case 9:
-                    System.out.println(30);
-                    break;
-                case 10:
-                    System.out.println(31);
-                    break;
-                case 11:
-                    System.out.println(30);
-                    break;
-                case 12:
-                    System.out.println(31);
-                    break;
-                default:
-                    System.out.println("invalid date");
-                    break;
+            if (year % 100 == 0) {
+            if (year % 400 == 0) {
+
+                switch (month) {
+                    case 1:
+                        System.out.println(31);
+                        break;
+                    case 2:
+                        System.out.println(28);
+                        break;
+                    case 3:
+                        System.out.println(31);
+                        break;
+                    case 4:
+                        System.out.println(30);
+                        break;
+                    case 5:
+                        System.out.println(31);
+                        break;
+                    case 6:
+                        System.out.println(30);
+                        break;
+                    case 7:
+                        System.out.println(31);
+                        break;
+                    case 8:
+                        System.out.println(31);
+                        break;
+                    case 9:
+                        System.out.println(30);
+                        break;
+                    case 10:
+                        System.out.println(31);
+                        break;
+                    case 11:
+                        System.out.println(30);
+                        break;
+                    case 12:
+                        System.out.println(31);
+                        break;
+                    default:
+                        System.out.println("invalid date");
+                        break;
+                }
+            }
             }
         }
         else {
             switch (month) {
                 case 1:
-                    System.out.println(31);
+                System.out.println(31);
                     break;
                 case 2:
                     System.out.println(29);
@@ -90,6 +95,9 @@ public class DaysInMonth {
                     break;
             }
         }
+        }
+        else {
+            System.out.println("invalid date");
         }
     }
 }
